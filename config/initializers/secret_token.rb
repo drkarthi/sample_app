1d3b308582beacbb1dc43a40688c071d2f991751
karthikthegoth <karthikthegoth@gmail.com>
2014-05-07 10:57:08 +0530

    Add a StaticPages controller

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,6 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+SampleApp::Application.config.secret_token = '2a6465d7514f1d66b5fb6a92da48f06825bcddd3d52a5616f3124ed05ba244db1f36776508381b059253d20cff412cf616d43cc834ff747c9510b7513de8bd32'
+
 require 'securerandom'
 
 def secure_token
